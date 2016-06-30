76f320a1fae6bbff5e3080c875cf2632d36307a6
Amadou Crookes <ascrookes@gmail.com>
2016-06-29 23:40:42 -0400

    added the ip address as a field that is logged

--- lib/logster/plugs/logger.ex
+++ lib/logster/plugs/logger.ex
@@ -43,7 +43,7 @@ defmodule Logster.Plugs.Logger do
         |> Keyword.put(:params, filter_params(conn.params))
         |> Keyword.put(:status, conn.status)
         |> Keyword.put(:duration, formatted_duration(duration))
-        |> Keyword.put(:state, conn.state)
+        |> Keyword.put(:ip, conn.remote_ip)
         |> formatter.format
       end
       conn
