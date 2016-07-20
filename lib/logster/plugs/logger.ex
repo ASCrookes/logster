29860bcf2c98286df180add2188f4f3412ae58f3
Amadou Crookes <ascrookes@gmail.com>
2016-07-20 15:53:16 -0400

    Log the user_id if it exists

--- lib/logster/plugs/logger.ex
+++ lib/logster/plugs/logger.ex
@@ -38,9 +38,11 @@ defmodule Logster.Plugs.Logger do
         duration = time_diff(start_time, stop_time)
         kl = []
         |> Keyword.put(:params, filter_params(conn.params))
-        |> Keyword.put(:status, conn.status)
         kl = if conn.remote_ip == nil, do: kl, else: Keyword.put(kl, :ip, conn.remote_ip)
+        user_id = Dict.get(conn.assigns, :user_id)
+        kl = if user_id, do: Keyword.put(kl, :user_id, user_id), else: kl
         kl
+        |> Keyword.put(:status, conn.status)
         |> Keyword.put(:duration, formatted_duration(duration))
         |> Keyword.put(:method, conn.method)
         |> Keyword.put(:path, conn.request_path)
